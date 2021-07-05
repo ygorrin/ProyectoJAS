0bfdc042090a3c0042e451105e0f1d3426edd2d0
Yamy <yamilagorrin@gmail.com>
2021-07-05 17:48:57 -0400

    Creacion de Proyecto JAS Coneccion Java Angular Spring Boot

---
+++ ProyectoJAS/src/test/java/com/yamy/app/ProyectoJasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoJasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
